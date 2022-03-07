1dbd63ac714fd33e0068fc737083a3bbe1810e9b
biggieP <100060522+biggieP@users.noreply.github.com>
2022-03-06 23:28:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ for( let x = 0; x < columns; x++ ) {
 
 
 const draw = () => {
-	context.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
+	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	context.fillStyle = '#9400d3';
+	context.fillStyle = 'hsl(' + 360 * Math.random() + ', 100%, 50%)';
 	context.font = fontSize + 'px monospace';
 
 	for(let i = 0; i < rainDrops.length; i++)
